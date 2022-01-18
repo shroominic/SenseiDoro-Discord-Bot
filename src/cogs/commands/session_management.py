3a7c2e8f53f9e4ad6bafe2cc1e9e9a25df12075b
DrNatoor <db2002dominic@gmail.com>
2022-01-18 23:28:49 +0100

    adding session command

--- src/cogs/commands/session_management.py
+++ src/cogs/commands/session_management.py
@@ -1,3 +1,5 @@
+import json
+
 from discord.ext import commands
 import asyncio
 
@@ -28,3 +30,20 @@ class SessionManagement(commands.Cog):
         self.bot.sessions.append(new_session)
         # initializes the session category and channels
         await new_session.create_environment()
+
+    @commands.command()
+    async def session(self, ctx, session_command=""):
+        """
+        Get control over your session:
+        :param ctx: context of command
+        :param session_command: use "delete" or "reset" to manage your session
+        """
+        # get session instance
+        session = await Session.get_session(ctx.channel, self.bot)
+
+        if "delete" in session_command:
+            await session.dispose()
+        if "reset" in session_command:
+            await session.reset_session()
+        else:
+            await ctx.send("Type '$session delete' or '$session reset'")
