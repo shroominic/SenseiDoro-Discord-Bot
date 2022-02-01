beeb67c24ab9b56ae19087812695b24dd949e5f8
DrNatoor <db2002dominic@gmail.com>
2022-02-01 21:03:38 +0100

    config only for admins

--- src/cogs/slash_cmds/config.py
+++ src/cogs/slash_cmds/config.py
@@ -12,10 +12,17 @@ class Config(SlashCommandGroup):
 
     @slash_command()
     async def mute_admins(self, ctx, value: bool):
-        # get dojo reference
-        dojo = self.bot.dojos[ctx.guild.id]
-        dojo.mute_admins = value
-        # feedback
-        title = "Config changed"
-        feedback = f"[mute_admins] <- {value}"
-        asyncio.create_task(cmd_helper.feedback(ctx, title, feedback))
+        # cmd only for admins
+        role = self.bot.dojos[ctx.guild.id].admin_role
+        if role in ctx.author.roles:
+            # get dojo reference
+            dojo = self.bot.dojos[ctx.guild.id]
+            dojo.mute_admins = value
+            # feedback
+            title = "Config changed"
+            feedback = f"[mute_admins] <- {value}"
+            asyncio.create_task(cmd_helper.feedback(ctx, title, feedback))
+        else:
+            title = "Missing Role"
+            feedback = "You are missing the admin role to run this command."
+            asyncio.create_task(cmd_helper.feedback(ctx, title, feedback))
