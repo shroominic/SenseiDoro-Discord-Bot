444ecd4b46f6af1acfe2f2bc7a73b5e624a34ff1
DrNatoor <db2002dominic@gmail.com>
2022-02-01 21:02:49 +0100

    small improvements

--- src/session/tools.py
+++ src/session/tools.py
@@ -4,6 +4,8 @@ async def get_session(some_channel, bot):
     dojo = bot.dojos[some_channel.guild.id]
     if some_channel.category.id in dojo.sessions:
         return dojo.sessions[some_channel.category.id]
+    elif len(dojo.sessions) == 1:
+        return list(dojo.sessions.values())[0]
     else:
         return None
 
