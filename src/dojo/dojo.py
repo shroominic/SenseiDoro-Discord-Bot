2026de2269cf403c89987ead3f66e5ca31db1592
DrNatoor <db2002dominic@gmail.com>
2022-02-03 12:58:11 +0100

    remove dojo_manager role

--- src/dojo/dojo.py
+++ src/dojo/dojo.py
@@ -13,14 +13,5 @@ class Dojo:
         # roles
         self.admin_role = None
         self.moderator_role = None
-        self.dojo_manager_role = None
         # configuration
         self.mute_admins = True
-        # async init
-        asyncio.create_task(self.init_async())
-
-    async def init_async(self):
-        if not get(self.guild.roles, name="Dojo Manager"):
-            self.dojo_manager_role = await self.guild.create_role(name="Dojo Manager")
-        else:
-            self.dojo_manager_role = get(self.guild.roles, name="Dojo Manager")
