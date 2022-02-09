ec5f6e7e2cbefb04dd89748932e51283f52a6d69
DrNatoor <db2002dominic@gmail.com>
2022-02-09 17:52:33 +0100

    decorators for role checking and cmd feedback

--- src/cogs/slash_cmds/config.py
+++ src/cogs/slash_cmds/config.py
@@ -4,34 +4,26 @@ from contextlib import closing
 
 from discord import SlashCommandGroup, slash_command
 
-from src.cogs.slash_cmds import cmd_helper
+from src.cogs.useful_decoration import admin_required, default_feedback
 
 
 class Config(SlashCommandGroup):
     def __init__(self, bot):
-        super().__init__(name="config", description="bot configuration")
+        super().__init__(name="config", description="[Admin] Configure special settings for Sensei Doro.")
         self.bot = bot
 
     @slash_command()
+    @admin_required
+    @default_feedback(title="Config changed", description="")
     async def mute_admins(self, ctx, value: bool):
-        # cmd only for admins
+        # get dojo reference
         dojo = self.bot.dojos[ctx.guild.id]
-        role = dojo.admin_role
-        if role in ctx.author.roles:
-            # get dojo reference
-            dojo = self.bot.dojos[ctx.guild.id]
-            dojo.mute_admins = value
-            with closing(sqlite3.connect("src/dbm/sensei.db")) as conn:
-                c = conn.cursor()
-                c.execute("UPDATE dojos SET cfg_mute_admins = :cfg WHERE id = :id",
-                          {"cfg": int(value), "id": ctx.guild.id})
-                conn.commit()
-            conn.close()
-            # feedback
-            title = "Config changed"
-            feedback = f"[mute_admins] <- {value}"
-            asyncio.create_task(cmd_helper.feedback(ctx, title, feedback))
-        else:
-            title = "Missing Role"
-            feedback = "You are missing the admin role to run this command."
-            asyncio.create_task(cmd_helper.feedback(ctx, title, feedback))
+        # update instance
+        dojo.mute_admins = value
+        # update database
+        with closing(sqlite3.connect("src/dbm/sensei.db")) as conn:
+            c = conn.cursor()
+            c.execute("UPDATE dojos SET cfg_mute_admins = :cfg WHERE id = :id",
+                      {"cfg": int(value), "id": ctx.guild.id})
+            conn.commit()
+
---
+++ src/cogs/useful_decoration.py
@@ -0,0 +1,67 @@
+import asyncio
+import functools
+
+from src.cogs.better_response import response
+
+
+def default_feedback(title, description="", seconds_visible=10):
+    def _default_feedback(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            # function application
+            result = await func(*args, **kwargs)
+            # feedback
+            ctx = args[1]
+            asyncio.create_task(response(ctx, title, description, seconds_visible))
+            # func return
+            return result
+        return wrapper
+    return _default_feedback
+
+
+def admin_required(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        self = args[0]
+        ctx = args[1]
+        # only with admin role
+        dojo = self.bot.dojos[ctx.guild.id]
+        role = dojo.admin_role
+        # if user has the role
+        if role in ctx.author.roles:
+            # function application
+            return await func(*args, **kwargs)
+        # if role not set
+        elif not role:
+            title = "Admin role not set"
+            feedback = "Type `/role admin @YOUR_ADMIN_ROLE` to use this command."
+            asyncio.create_task(response(ctx, title, feedback))
+        else:
+            title = "Missing Role"
+            feedback = f"Only user with @{role.name} can run this command."
+            asyncio.create_task(response(ctx, title, feedback))
+    return wrapper
+
+
+def mod_required(func):
+    @functools.wraps(func)
+    async def wrapper(*args, **kwargs):
+        self = args[0]
+        ctx = args[1]
+        # only with mod role
+        dojo = self.bot.dojos[ctx.guild.id]
+        role = dojo.mod_role
+        # if user has the role
+        if role in ctx.author.roles:
+            # function application
+            return await func(*args, **kwargs)
+        # if role not set
+        elif not role:
+            title = "Moderator role not set"
+            feedback = "Type `/role moderator @YOUR_MOD_ROLE` to use this command."
+            asyncio.create_task(response(ctx, title, feedback))
+        else:
+            title = "Missing Role"
+            feedback = f"Only user with @{role.name} can run this command."
+            asyncio.create_task(response(ctx, title, feedback))
+    return wrapper
