00c973f70231d31ad589fe4b56885c3ef473a7ea
DrNatoor <db2002dominic@gmail.com>
2022-02-21 08:22:47 +0100

    insert only_admin_debug decorators

--- src/cogs/slash_cmds/admin_tools.py
+++ src/cogs/slash_cmds/admin_tools.py
@@ -20,16 +20,16 @@ class DebugTools(commands.Cog):
             response(ctx, title)
 
     @debug.group()
-    async def doro(self, ctx):
+    async def info(self, ctx):
         """ doro cmd group """
         if ctx.invoked_subcommand is None:
             # command response
             title = "Wrong argument"
             response(ctx, title)
 
-    @doro.command()
+    @info.command()
     @only_admin_debug
-    async def running_sessions(self, ctx):
+    async def sessions(self, ctx):
         msg = discord.Embed(title="Run Information")
         active_sessions = 0
         active_members = 0
@@ -54,45 +54,42 @@ class DebugTools(commands.Cog):
             response(ctx, title)
 
     @delete.command()
+    @only_admin_debug
     async def envs(self, ctx):
         """ Removes all environments from the bot. """
-        # cmd only for me
-        if ctx.author.id == 302:
-            title = "Okay sir I'll clean your room!"
+        title = "Okay sir I'll clean your room!"
         response(ctx, title)
-            # get dojo reference
-            for category in ctx.guild.categories:
-                if "🍅" in category.name:
-                    # delete all channels inside category
-                    for vc in category.voice_channels:
-                        await vc.delete()
-                    for tc in category.text_channels:
-                        await tc.delete()
-                    # delete category
-                    await category.delete()
+        # get dojo reference
+        for category in ctx.guild.categories:
+            if "🍅" in category.name:
+                # delete all channels inside category
+                for vc in category.voice_channels:
+                    await vc.delete()
+                for tc in category.text_channels:
+                    await tc.delete()
+                # delete category
+                await category.delete()
 
     @delete.command()
+    @only_admin_debug
     async def messages(self, ctx):
         """ Removes all messages from the channel. """
-        # cmd only for me
-        if ctx.author.id == 302:
-            # delete all messages inside message.channel
-            async for msg in ctx.channel.history():
-                asyncio.create_task(msg.delete())
-            # command response
-            title = "Okay sir I'll delete your messages!"
-            asyncio.create_task(response(ctx, title))
+        # delete all messages inside message.channel
+        async for msg in ctx.channel.history():
+            asyncio.create_task(msg.delete())
+        # command response
+        title = "Okay sir I'll delete your messages!"
+        response(ctx, title)
 
     @delete.command()
+    @only_admin_debug
     async def sessions(self, ctx):
         """ Removes all Session environments from the bot. """
-        # cmd only for me
-        if ctx.author.id == 302:
-            # command response
-            title = "Okay sir I'll delete your sessions!"
+        # command response
+        title = "Okay sir I'll delete your sessions!"
         response(ctx, title)
-            # get dojo reference
-            dojo = self.bot.dojos[ctx.guild.id]
-            # delete all sessions in ctx.guild
-            for session in dojo.sessions.values():
-                asyncio.create_task(session.dispose())
+        # get dojo reference
+        dojo = self.bot.dojos[ctx.guild.id]
+        # delete all sessions in ctx.guild
+        for session in dojo.sessions.values():
+            asyncio.create_task(session.dispose())
